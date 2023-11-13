b855b15b8ab4209b2767c79cf9d8b6cea78dd33c
James Lu <james@overdrivenetworks.com>
2023-11-13 12:49:56 -0800

    Polyfill `for await` on Chrome

--- static/webtrace.js
+++ static/webtrace.js
@@ -9,6 +9,21 @@ const updateWorkingStatus = () => {
 }
 let activeLoop = null;
 
+// Polyfill for `for await`, from https://bugs.chromium.org/p/chromium/issues/detail?id=929585#c10
+ReadableStream.prototype[Symbol.asyncIterator] = async function* () {
+    const reader = this.getReader()
+    try {
+      while (true) {
+        const {done, value} = await reader.read()
+        if (done) return
+        yield value
+      }
+    }
+    finally {
+      reader.releaseLock()
+    }
+  }
+
 async function stopTrace(keepOutput) {
     if (activeLoop) {
         console.debug(`Clearing old trace loop ${activeLoop}`);
